6ad9b8d3f4d48c1f6d8fb76a9ddb6a5a88252fcc
Lida Strakh <strax69@gmail.com>
2021-11-25 09:14:45 +0000

    Clean up build script.
    Changes:
      - stop at first error (set -e)
      - factor out compile and link flags

--- make.sh
+++ make.sh
@@ -1,6 +1,12 @@
 set -x
+set -e
 
-g++ -g -Wall -Wextra -fsanitize=address -fsanitize=leak -O2 -c list.cpp -o list.o
-g++ -g -Wall -Wextra -fsanitize=address -fsanitize=leak -O2 -c print_num.cpp -o print_num.o
-g++ -g -Wall -Wextra -fsanitize=address -fsanitize=leak -O2 -c array.cpp -o array.o
-g++ -g -Wall -Wextra -fsanitize=address -fsanitize=leak -O2 -c main.cpp -o main.o && g++ -fsanitize=address -fsanitize=leak main.o array.o list.o print_num.o -o main
+cflags="-g -O2 -Wall -Wextra -fsanitize=address -fsanitize=leak"
+ldflags="-fsanitize=address -fsanitize=leak"
+
+g++ $cflags -c list.cpp -o list.o
+g++ $cflags -c print_num.cpp -o print_num.o
+g++ $cflags -c array.cpp -o array.o
+g++ $cflags -c main.cpp -o main.o 
+
+g++ $ldflags main.o array.o list.o print_num.o -o main
