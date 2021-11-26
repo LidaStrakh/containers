c49d00fb2d40baf6a081dc1cc6c6fea40e222abd
Lida Strakh <strax69@gmail.com>
2021-11-26 08:14:41 +0000

    Use for loop to avoid repetition.

--- make.sh
+++ make.sh
@@ -4,9 +4,8 @@ set -e
 cflags="-g -O2 -Wall -Wextra -fsanitize=address -fsanitize=leak"
 ldflags="-fsanitize=address -fsanitize=leak"
 
-g++ $cflags -c list.cpp -o list.o
-g++ $cflags -c print_num.cpp -o print_num.o
-g++ $cflags -c array.cpp -o array.o
-g++ $cflags -c main.cpp -o main.o 
+for f in list print_num array main ; do
+  g++ $cflags -c $f.cpp -o $f.o
+done 
 
 g++ $ldflags main.o array.o list.o print_num.o -o main
